c1f3f99f1e2c7423f92aff30395c06c4bda2c593
guddnboy <hung3018@naver.com>
2024-09-20 17:08:08 +0900

    refact: 복잡하고 얽혀있는 기능, 페이지 삭제

--- src/store/messageSlice.tsx
+++
@@ -1,9 +0,0 @@
-export interface MessageSlice {
-    username: string;
-    message: string;
-    time: string;
-
-    setUsername: (username: string) => void;
-    setMessage: (message: string) => void;
-    setTime: (time: string) => void;
-}
