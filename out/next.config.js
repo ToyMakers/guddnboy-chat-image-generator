7e2c32a09c1d7f68b81b03d130e0dc46965c2553
guddnboy <hung3018@naver.com>
2024-09-20 17:50:46 +0900

    Deploy

--- out/next.config.js
+++ out/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     output: "export",
     basepath: "/chat-image-generator",
+    assetPrefix:
+        process.env.NODE_ENV === "production"
+            ? "https://guddnboy.github.io/chat-image-generator/"
+            : "",
 };
