1034ab5a49628c206571368939b43efdf046a365
guddnboy <hung3018@naver.com>
2024-09-24 15:19:48 +0900

    feat: 메세지 추가 시 UserForm 추가

--- src/components/Add/AddUser.tsx
+++ src/components/Add/AddUser.tsx
@@ -1,57 +1,31 @@
 "use client";
-import React from "react";
-import { useState } from "react";
-import CreateModal from "../Modal/createModal";
+import React, { useState } from "react";
+import UserForm from "../UserForm";
 
 const AddUser = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const handleModalOpen = () => {
-        setIsOpen(true);
-    };
-    const handleModalClose = () => {
-        setIsOpen(false);
+    const [userForms, setUserForms] = useState([0]);
+
+    const addUserForm = () => {
+        setUserForms((prev) => [...prev, prev.length]);
     };
 
     return (
         <div className="w-[620px] h-auto bg-yourchatbg rounded-md p-4">
-            <h2>프로필 생성 페이지</h2>
             <div>
-                <section className="flex justify-between h-10 items-center ">
-                    <div>
-                        <select id="name">
-                            <option value="고양이">고양이</option>
-                            <option value="강아지">강아지</option>
-                        </select>
-                    </div>
-                    <div className="">
-                        <input
-                            type="text"
-                            placeholder="메세지를 입력하세요"
-                            className="px-2"
-                        />
-                    </div>
-                    <div>
-                        <input type="time" />
-                        시간
-                    </div>
-                    <div>
-                        <button> remove</button>
-                    </div>
+                <section className="flex justify-between">
+                    <div className="w-32 h-8 mb-2 text-left">말 한 사람</div>
+                    <div className="w-48 h-8 mb-2 text-left pl-2">내용</div>
+                    <div className="w-32 h-8 mb-2 text-left">시간</div>
                 </section>
-                <button
-                    onClick={handleModalOpen}
-                    className="w-[580px] bg-chatbg text-white py-2 px-4 rounded-md mb-2">
-                    추가
-                </button>
+                {userForms.map((_, index) => (
+                    <UserForm key={index} userIndex={index} />
+                ))}
                 <div className="">
-                    {isOpen && (
-                        <div className="w-full h-full fixed">
-                            <CreateModal
-                                title={"새 사용자 추가하기"}
-                                handleClose={handleModalClose}
-                            />
-                        </div>
-                    )}
+                    <button
+                        onClick={addUserForm}
+                        className="w-[580px] bg-chatbg text-white py-2 px-4 rounded-md mb-2">
+                        메세지 추가
+                    </button>
                 </div>
             </div>
         </div>
