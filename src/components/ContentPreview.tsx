0a001eec91271e2883507fae4652f191d917c2a3
guddnboy <hung3018@naver.com>
2024-09-27 16:28:26 +0900

    feat: 수정 페이지 이동 기능추가

--- src/components/ContentPreview.tsx
+++ src/components/ContentPreview.tsx
@@ -2,13 +2,43 @@
 import React from "react";
 import UserMessagePreview from "./UserMessagePreview";
 import { useUserStore } from "../store/useUserStore";
+import {
+  useModifyBackgroundStore,
+  useModifyMessageStore,
+  useModifyNameStore,
+  useModifyTimeStore,
+  useModifyProfileStore,
+} from "../store/useModifyBooleanStore";
 
 const ContentPreview = () => {
   const users = useUserStore((state) => state.users);
-  console.log(users);
+
+  const setIsModifyName = useModifyNameStore((state) => state.setIsModifyName);
+  const setIsModifyMessage = useModifyMessageStore(
+    (state) => state.setIsModifyMessage
+  );
+  const setIsModifyTime = useModifyTimeStore((state) => state.setIsModifyTime);
+  const setIsModifyBackground = useModifyBackgroundStore(
+    (state) => state.setIsModifyBackground
+  );
+  const setIsModifyProfile = useModifyProfileStore(
+    (state) => state.setIsModifyProfile
+  );
+
+  const goToModifyBackground = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.currentTarget === e.target) {
+      setIsModifyBackground(true);
+      setIsModifyMessage(false);
+      setIsModifyName(false);
+      setIsModifyTime(false);
+      setIsModifyProfile(false);
+    }
+  };
 
   return (
-    <div className="w-80 h-96 border border-solid rounded-md border-slate-400 overflow-y-scroll">
+    <div
+      className="w-80 h-96 border border-solid rounded-md border-slate-400 overflow-y-scroll hover:border hover:border-gray-500"
+      onClick={goToModifyBackground}>
       <div className="flex flex-col">
         {users.map((user, index) => (
           <div key={index} className="flex my-1 p-2 border-b border-gray-300">
--- src/components/UserMessagePreview.tsx
+++ src/components/UserMessagePreview.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import defaultProfile from "../../public/images/default.png";
+import {
+  useModifyMessageStore,
+  useModifyNameStore,
+  useModifyTimeStore,
+  useModifyBackgroundStore,
+  useModifyProfileStore,
+} from "@/store/useModifyBooleanStore";
 
 const UserMessagePreview = ({
   username,
@@ -11,19 +18,75 @@ const UserMessagePreview = ({
   message: string;
   time: string;
 }) => {
+  const setIsModifyName = useModifyNameStore((state) => state.setIsModifyName);
+  const setIsModifyMessage = useModifyMessageStore(
+    (state) => state.setIsModifyMessage
+  );
+  const setIsModifyTime = useModifyTimeStore((state) => state.setIsModifyTime);
+  const setIsModifyBackground = useModifyBackgroundStore(
+    (state) => state.setIsModifyBackground
+  );
+  const setIsModifyProfile = useModifyProfileStore(
+    (state) => state.setIsModifyProfile
+  );
+
+  const handleModifyName = () => {
+    setIsModifyBackground(false);
+    setIsModifyMessage(false);
+    setIsModifyName(true);
+    setIsModifyTime(false);
+    setIsModifyProfile(false);
+  };
+
+  const handleModifyMessage = () => {
+    setIsModifyBackground(false);
+    setIsModifyMessage(true);
+    setIsModifyName(false);
+    setIsModifyTime(false);
+    setIsModifyProfile(false);
+  };
+
+  const handleModifyTime = () => {
+    setIsModifyBackground(false);
+    setIsModifyMessage(false);
+    setIsModifyName(false);
+    setIsModifyTime(true);
+    setIsModifyProfile(false);
+  };
+
+  const handleModifyProfile = () => {
+    setIsModifyBackground(false);
+    setIsModifyMessage(false);
+    setIsModifyName(false);
+    setIsModifyTime(false);
+    setIsModifyProfile(true);
+  };
+
   return (
     <div className="flex text-sm">
-      <div className="flex justify-center items-center align-middle overflow-hidden">
+      <div
+        className="flex size-10 justify-center items-center align-middle hover:cursor-pointer hover:border border-solid border-slate-500 rounded-full"
+        onClick={handleModifyProfile}>
         <Image
           className="text-sm size-10 rounded-full"
           src={defaultProfile}
           alt="프로필"></Image>
       </div>
-      <div className="text-sm w-10">{username}</div>
-      <div className="flex items-center w-auto h-auto px-2 m-1 bg-yourchatbg text-sm rounded-md">
+      <div
+        className="text-sm w-10 h-6 hover:cursor-pointer hover:border border-solid border-slate-500 rounded-md"
+        onClick={handleModifyName}>
+        {username}
+      </div>
+      <div
+        className="flex items-center w-auto h-auto px-2 m-1 bg-yourchatbg text-sm rounded-md hover:cursor-pointer hover:border border-solid border-slate-500"
+        onClick={handleModifyMessage}>
         {message}
       </div>
-      <div className="flex items-end">{time}</div>
+      <div
+        className="flex items-end hover:cursor-pointer hover:border border-solid border-slate-500"
+        onClick={handleModifyTime}>
+        {time}
+      </div>
     </div>
   );
 };
