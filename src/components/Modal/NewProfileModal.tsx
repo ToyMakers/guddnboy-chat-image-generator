8308d4f28770154e9ed8b4e2e70ce772fa8bd497
guddnboy <hung3018@naver.com>
2024-10-01 12:59:09 +0900

    feat: 프로필 추가 기능

--- src/components/Modal/NewProfileModal.tsx
+++ src/components/Modal/NewProfileModal.tsx
@@ -1,9 +1,10 @@
 "use client";
-// import { useFormsStore } from "@/store/useFormsStore";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import { useProfileStore } from "@/store/useUserFormStore";
+import defaultImage from "../../../public/images/default.png";
 
-const CreateModal = ({
+const NewProfileModal = ({
   title,
   handleClose,
 }: {
@@ -11,6 +12,19 @@ const CreateModal = ({
   handleClose: () => void;
 }) => {
   const [name, setName] = useState("");
+  const [profileImage, setProfileImage] = useState<File | null>();
+
+  const defaultImageFile = new File([defaultImage.src], "default.png", {
+    type: "image/png",
+  });
+
+  const addProfile = useProfileStore((state) => state.addProfile);
+  const profiles = useProfileStore((state) => state.profiles);
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setProfileImage(e.target.files[0]);
+  };
 
   const handleAddUser = () => {
     if (!name) {
@@ -21,9 +35,24 @@ const CreateModal = ({
       alert("이름은 5글자 이하로 입력해주세요.");
       return;
     }
-
+    addProfile(profiles.length, profileImage ?? defaultImageFile, name);
+    console.log("profiles : ", profiles);
     handleClose();
   };
+
+  useEffect(() => {
+    if (profileImage) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64data = reader.result as string;
+        const newProfileImageFile = new File([base64data], profileImage.name, {
+          type: profileImage.type,
+        });
+        setProfileImage(newProfileImageFile);
+      };
+      reader.readAsDataURL(profileImage);
+    }
+  }, [profileImage]);
 
   return (
     <div className="flex fixed inset-0 z-0 bg-gray-700 bg-opacity-70 items-center justify-center">
@@ -33,7 +62,6 @@ const CreateModal = ({
             X
           </button>
         </div>
-
         <h2 className="text-2xl font-bold mb-8">{title}</h2>
         <section className="w-[420px] h-[180px]">
           <div className="flex justify-between items-center">
@@ -41,7 +69,6 @@ const CreateModal = ({
             <input
               type="text"
               placeholder="이름 입력"
-              id="name"
               value={name}
               onChange={(e) => setName(e.target.value)}
               className="w-72 border p-4 rounded-md"
@@ -51,17 +78,14 @@ const CreateModal = ({
             프로필 사진
             <input
               type="file"
-              id="profile"
-              onChange={() => {}}
+              onChange={onFileChange}
               className="w-72 border p-4 rounded-md"
             />
           </div>
         </section>
         <div className="mt-4">
           <button
-            onClick={() => {
-              handleAddUser();
-            }}
+            onClick={handleAddUser}
             className="px-4 py-2 bg-chatbg text-white rounded-md transition hover:bg-sky-700">
             추가하기
           </button>
@@ -71,4 +95,4 @@ const CreateModal = ({
   );
 };
 
-export default CreateModal;
+export default NewProfileModal;
