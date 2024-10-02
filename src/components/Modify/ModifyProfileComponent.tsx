62c026e7b3519a37c2b58fb14395afc16087ff72
guddnboy <hung3018@naver.com>
2024-10-02 14:09:36 +0900

    refact: 프로필, 이름 place홀더 값 표시, set함수 handle함수로 감싸기

--- src/components/Modify/ModifyProfileComponent.tsx
+++ src/components/Modify/ModifyProfileComponent.tsx
@@ -6,6 +6,14 @@ import { useProfileStyleStore } from "@/store/ModifyStyle/useProfileStyleStore";
 const ModifyProfileComponent = () => {
   const {
     width,
+    height,
+    backgroundColor,
+    borderWidth,
+    borderRadius,
+    padding,
+    borderColor,
+    borderStyle,
+
     setWidth,
     setHeight,
     setBackgroundColor,
@@ -16,58 +24,114 @@ const ModifyProfileComponent = () => {
     setBorderStyle,
   } = useProfileStyleStore();
 
+  const handleWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setWidth(Number(value));
+  };
+  const handleHeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setHeight(Number(value));
+  };
+
+  const handleBackgroundColorChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setBackgroundColor(e.target.value);
+  };
+
+  const handleBorderWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBorderWidth(Number(e.target.value));
+  };
+
+  const handleBorderRadiusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBorderRadius(Number(e.target.value));
+  };
+
+  const handlePaddingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPadding(Number(e.target.value));
+  };
+
+  const handleBorderColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBorderColor(e.target.value);
+  };
+
+  const handleBorderStyleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBorderStyle(e.target.value);
+  };
+
   return (
     <div className="flex flex-col w-full h-80 overflow-scroll">
       <div>
         너비
         <input
+          id="width"
           type="number"
           value={width}
-          onChange={(e) => setWidth(Number(e.target.value))}
+          onChange={handleWidthChange}
         />
       </div>
       <div>
         높이
         <input
+          id="height"
           type="number"
-          onChange={(e) => setHeight(Number(e.target.value))}
+          value={height}
+          onChange={handleHeightChange}
         />
       </div>
       <div>
         배경색
         <input
+          id="backgroundColor"
           type="color"
-          onChange={(e) => setBackgroundColor(e.target.value)}
+          value={backgroundColor}
+          onChange={handleBackgroundColorChange}
         />
       </div>
       <div>
         테두리 두께
         <input
+          id="borderWidth"
           type="number"
-          onChange={(e) => setBorderWidth(Number(e.target.value))}
+          value={borderWidth}
+          onChange={handleBorderWidthChange}
         />
       </div>
       <div>
         테두리 모서리
         <input
+          id="borderRadius"
           type="number"
-          onChange={(e) => setBorderRadius(Number(e.target.value))}
+          value={borderRadius}
+          onChange={handleBorderRadiusChange}
         />
       </div>
       <div>
         패딩
         <input
+          id="padding"
           type="number"
-          onChange={(e) => setPadding(Number(e.target.value))}
+          value={padding}
+          onChange={handlePaddingChange}
         />
       </div>
       <div>
         테두리 색
-        <input type="color" onChange={(e) => setBorderColor(e.target.value)} />
+        <input
+          id="borderColor"
+          type="color"
+          value={borderColor}
+          onChange={handleBorderColorChange}
+        />
       </div>
       <div>
         테두리 스타일
-        <input type="text" onChange={(e) => setBorderStyle(e.target.value)} />
+        <input
+          id="borderStyle"
+          type="text"
+          value={borderStyle}
+          onChange={handleBorderStyleChange}
+        />
       </div>
     </div>
   );
