61a126a19a0ae0485091a50bc4ec8698ac2fbe7b
guddnboy <hung3018@naver.com>
2024-10-02 14:47:31 +0900

    design: 메세지 스타일 수정 컴포넌트 디자인

--- src/components/Modify/ModifyMessageComponent.tsx
+++ src/components/Modify/ModifyMessageComponent.tsx
@@ -22,67 +22,99 @@ const ModifyMessageComponent = () => {
   } = useMessageStyleStore();
 
   return (
-    <div className="w-full h-80 overflow-scroll">
-      <div>
-        글자크기
-        <input
-          type="number"
-          onChange={(e) => setFontSize(Number(e.target.value))}
-          placeholder={fontSize_Message.toString()}
-        />
-      </div>
-      <div>
-        글자두께
-        <input
-          type="number"
-          onChange={(e) => setFontWeight(Number(e.target.value))}
-          placeholder={fontWeight_Message.toString()}
-        />
-      </div>
-      <div>
-        글자 패딩
-        <input
-          type="number"
-          onChange={(e) => setFontPadding(Number(e.target.value))}
-          placeholder={fontPadding_Message.toString()}
-        />
-      </div>
-      <div>
-        글자색상
-        <input
-          type="color"
-          onChange={(e) => setFontColor(e.target.value)}
-          placeholder={fontColor_Message}
-        />
-      </div>
-      <div>
-        모서리 둥글기
-        <input
-          type="number"
-          onChange={(e) => setBorderRounded(Number(e.target.value))}
-          placeholder={borderRounded_Message.toString()}
-        />
-      </div>
-      <div>
-        배경색상
-        <input
-          type="color"
-          value={backgroundcolor_Message}
-          onChange={(e) => {
-            useMessageStyleStore.setState({
-              backgroundcolor_Message: e.target.value,
-            });
-          }}
-        />
-      </div>
-      <div>
-        너비
-        <input
-          type="number"
-          onChange={(e) => setWidth(Number(e.target.value))}
-          placeholder={width_Message.toString()}
-        />
-      </div>
+    <div className="flex flex-col text-left w-full h-80 overflow-scroll">
+      <section className="mb-4">
+        <div className="text-lg">글자크기</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            type="number"
+            onChange={(e) => setFontSize(Number(e.target.value))}
+            placeholder={fontSize_Message.toString()}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">글자두께</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            type="number"
+            onChange={(e) => setFontWeight(Number(e.target.value))}
+            placeholder={fontWeight_Message.toString()}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+      <section className="mb-4">
+        <div className="text-lg">글자 패딩</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            type="number"
+            onChange={(e) => setFontPadding(Number(e.target.value))}
+            placeholder={fontPadding_Message.toString()}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">글자색상</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-28 pl-2 bg-inherit"
+            type="color"
+            onChange={(e) => setFontColor(e.target.value)}
+            placeholder={fontColor_Message}
+          />
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">모서리 둥글기 </div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            type="number"
+            onChange={(e) => setBorderRounded(Number(e.target.value))}
+            placeholder={borderRounded_Message.toString()}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">배경색상</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-28 pl-2 bg-inherit"
+            type="color"
+            value={backgroundcolor_Message}
+            onChange={(e) => {
+              useMessageStyleStore.setState({
+                backgroundcolor_Message: e.target.value,
+              });
+            }}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">너비</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            type="number"
+            onChange={(e) => setWidth(Number(e.target.value))}
+            placeholder={width_Message.toString()}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
     </div>
   );
 };
