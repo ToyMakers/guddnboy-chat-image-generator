995cec12fc88c630bd5703cb47f3e2073ccf013c
guddnboy <hung3018@naver.com>
2024-10-02 14:58:07 +0900

    design: 프로필 스타일 수정 컴포넌트 디자인 추가

--- src/components/Modify/ModifyProfileComponent.tsx
+++ src/components/Modify/ModifyProfileComponent.tsx
@@ -60,79 +60,118 @@ const ModifyProfileComponent = () => {
   };
 
   return (
-    <div className="flex flex-col w-full h-80 overflow-scroll">
-      <div>
-        너비
-        <input
-          id="width"
-          type="number"
-          value={width}
-          onChange={handleWidthChange}
-        />
-      </div>
-      <div>
-        높이
-        <input
-          id="height"
-          type="number"
-          value={height}
-          onChange={handleHeightChange}
-        />
-      </div>
-      <div>
-        배경색
-        <input
-          id="backgroundColor"
-          type="color"
-          value={backgroundColor}
-          onChange={handleBackgroundColorChange}
-        />
-      </div>
-      <div>
-        테두리 두께
-        <input
-          id="borderWidth"
-          type="number"
-          value={borderWidth}
-          onChange={handleBorderWidthChange}
-        />
-      </div>
-      <div>
-        테두리 모서리
-        <input
-          id="borderRadius"
-          type="number"
-          value={borderRadius}
-          onChange={handleBorderRadiusChange}
-        />
-      </div>
-      <div>
-        패딩
-        <input
-          id="padding"
-          type="number"
-          value={padding}
-          onChange={handlePaddingChange}
-        />
-      </div>
-      <div>
-        테두리 색
-        <input
-          id="borderColor"
-          type="color"
-          value={borderColor}
-          onChange={handleBorderColorChange}
-        />
-      </div>
-      <div>
-        테두리 스타일
-        <input
-          id="borderStyle"
-          type="text"
-          value={borderStyle}
-          onChange={handleBorderStyleChange}
-        />
-      </div>
+    <div className="flex flex-col text-left w-full h-80 overflow-scroll">
+      <section className="mb-4">
+        <div className="text-lg">너비</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            id="width"
+            type="number"
+            value={width}
+            onChange={handleWidthChange}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">높이</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            id="height"
+            type="number"
+            value={height}
+            onChange={handleHeightChange}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">배경색</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-28 pl-2 bg-inherit"
+            id="backgroundColor"
+            type="color"
+            value={backgroundColor}
+            onChange={handleBackgroundColorChange}
+          />
+          <div className="w-12 text-center text-timebg">T</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">테두리 두께</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            id="borderWidth"
+            type="number"
+            value={borderWidth}
+            onChange={handleBorderWidthChange}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">테두리 모서리</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            id="borderRadius"
+            type="number"
+            value={borderRadius}
+            onChange={handleBorderRadiusChange}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">패딩</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            id="padding"
+            type="number"
+            value={padding}
+            onChange={handlePaddingChange}
+          />
+          <div className="w-12 text-center text-timebg">1</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">테두리 색</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-28 pl-2 bg-inherit"
+            id="borderColor"
+            type="color"
+            value={borderColor}
+            onChange={handleBorderColorChange}
+          />
+          <div className="w-12 text-center text-timebg">T</div>
+        </div>
+      </section>
+
+      <section className="mb-4">
+        <div className="text-lg">테두리 스타일</div>
+        <div className="flex items-center justify-between w-full h-10 border border-solid border-gray-400 rounded-md">
+          <input
+            className="w-11/12 h-full pl-2 bg-inherit outline-none"
+            id="borderStyle"
+            type="text"
+            value={borderStyle}
+            onChange={handleBorderStyleChange}
+          />
+          <div className="w-12 text-center text-timebg">T</div>
+        </div>
+      </section>
     </div>
   );
 };
