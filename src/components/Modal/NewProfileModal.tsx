938a566a2b3d51caefa5601e772a1d051c1c3ab3
guddnboy <hung3018@naver.com>
2024-10-05 11:46:23 +0900

    feat: 업로드한 이미지 미리보기

--- src/components/Modal/NewProfileModal.tsx
+++ src/components/Modal/NewProfileModal.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 import { useProfileStore } from "../../stores/useProfileStore";
 
 const NewProfileModal = ({
@@ -11,14 +11,16 @@ const NewProfileModal = ({
   handleClose: () => void;
 }) => {
   const [name, setName] = useState("");
-  const [profileImage, setProfileImage] = useState<File | null>();
+  const [profileImage, setProfileImage] = useState<File | null>(null);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
 
-  const addProfile = useProfileStore((state) => state.addProfile);
   const profiles = useProfileStore((state) => state.profiles);
+  const addProfile = useProfileStore((state) => state.addProfile);
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     setProfileImage(e.target.files[0]);
+    setPreviewImage(URL.createObjectURL(e.target.files[0]));
   };
 
   const handleAddUser = () => {
@@ -35,20 +37,6 @@ const NewProfileModal = ({
     handleClose();
   };
 
-  useEffect(() => {
-    if (profileImage) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const base64data = reader.result as string;
-        const newProfileImageFile = new File([base64data], profileImage.name, {
-          type: profileImage.type,
-        });
-        setProfileImage(newProfileImageFile);
-      };
-      reader.readAsDataURL(profileImage);
-    }
-  }, [profileImage]);
-
   return (
     <div className="flex fixed inset-0 z-0 bg-gray-700 bg-opacity-70 items-center justify-center">
       <div className="bg-white p-6 rounded-md">
@@ -58,6 +46,7 @@ const NewProfileModal = ({
           </button>
         </div>
         <h2 className="text-2xl font-bold mb-8">{title}</h2>
+
         <section className="w-[420px] h-[180px]">
           <div className="flex justify-between items-center">
             이름
@@ -77,6 +66,17 @@ const NewProfileModal = ({
               className="w-72 border p-4 rounded-md"
             />
           </div>
+          {previewImage && (
+            <div className="mt-4">
+              <Image
+                src={previewImage}
+                alt="프로필 미리보기"
+                width={50}
+                height={50}
+                className="rounded-md"
+              />
+            </div>
+          )}
         </section>
 
         <div className="mt-4">
