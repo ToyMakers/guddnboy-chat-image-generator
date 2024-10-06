597c1cebc59809bfe822adb0887335ca47d03111
guddnboy <hung3018@naver.com>
2024-10-06 22:55:01 +0900

    chore: 배포 시도

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://guddnboy.github.io/chat-image-generator/"
+    : "";
+
+const nextConfig = {
+  output: "export",
+  assetPrefix: prefix,
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "https://guddnboy.github.io/chat-image-generator/"
       : "",
 };
+
+module.exports = nextConfig; // nextConfig 변수를 올바르게 내보냅니다.
