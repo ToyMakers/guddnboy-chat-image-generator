b6a41d6e1b8f10952cb535546283eee7bf20be69
guddnboy <hung3018@naver.com>
2024-10-06 22:02:33 +0900

    chore: config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://guddnboy.github.io/chat-image-generator"
+    : "";
+
+const nextConfig = {
+  output: "export",
+  assetPrefix: prefix,
+};
 
 export default nextConfig;
