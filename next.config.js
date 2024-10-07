cc9030f9794163a4981858c9ccdca0b85546fc4f
guddnboy <hung3018@naver.com>
2024-10-07 19:43:10 +0900

    chore: change the output mode

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: ["api.dicebear.com"],
   },
+  output: "export",
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "https://guddnboy.github.io/chat-image-generator/"
