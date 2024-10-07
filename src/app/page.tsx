1e4154f03d68358b8ea28545c725bd9410573235
guddnboy <hung3018@naver.com>
2024-10-07 22:33:20 +0900

    feat: 이미지 저장, 클립보드 복사 시 로딩 창 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,64 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import html2canvas from "html2canvas";
 import ContentPreview from "../components/ContentPreview";
 import HeaderLayout from "../components/layout/HeaderLayout";
 import AddUser from "../components/add/AddUser";
+import { cardio } from "ldrs";
+cardio.register();
 
 export default function RootPage() {
   const contentPreviewRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState(false); // 로딩 상태 관리
 
   const captureContentPreview = async () => {
     if (contentPreviewRef.current) {
-      const canvas = await html2canvas(contentPreviewRef.current);
-      const imgData = canvas.toDataURL("image/png");
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      try {
+        const canvas = await html2canvas(contentPreviewRef.current);
+        const imgData = canvas.toDataURL("image/png");
 
-      const fileName = prompt("저장할 파일명을 입력하세요:", "미리보기.png");
+        const fileName = prompt("저장할 파일명을 입력하세요:", "미리보기.png");
 
-      if (fileName) {
-        const link = document.createElement("a");
-        link.href = imgData;
-        link.download = fileName;
-        link.click();
-      } else {
-        alert("파일명이 입력되지 않아 다운로드가 취소되었습니다.");
+        if (fileName) {
+          const link = document.createElement("a");
+          link.href = imgData;
+          link.download = fileName;
+          link.click();
+        } else {
+          alert("파일명이 입력되지 않아 다운로드가 취소되었습니다.");
+        }
+      } catch (error) {
+        console.error("캡처 오류:", error);
+        alert("이미지 캡처 중 오류가 발생했습니다.");
+      } finally {
+        setLoading(false);
       }
     }
   };
 
   const copyToClipboard = async () => {
     if (contentPreviewRef.current) {
-      const canvas = await html2canvas(contentPreviewRef.current);
-      canvas.toBlob(async (blob) => {
-        if (blob) {
-          try {
+      setLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      try {
+        const canvas = await html2canvas(contentPreviewRef.current);
+        canvas.toBlob(async (blob) => {
+          if (blob) {
             await navigator.clipboard.write([
               new ClipboardItem({ "image/png": blob }),
             ]);
             alert("이미지가 클립보드에 복사되었습니다.");
-          } catch (err) {
-            console.error("클립보드 복사 오류:", err);
-            alert("클립보드에 복사하는 데 실패했습니다.");
           }
-        }
-      }, "image/png");
+        }, "image/png");
+      } catch (err) {
+        console.error("클립보드 복사 오류:", err);
+        alert("클립보드에 복사하는 데 실패했습니다.");
+      } finally {
+        setLoading(false);
+      }
     }
   };
 
@@ -62,6 +78,11 @@ export default function RootPage() {
           <AddUser />
         </div>
       </div>
+      {loading && (
+        <div className=" fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50 z-50">
+          <l-cardio size="150" speed="1.75" color="#9bbbd4" stroke="13" />
+        </div>
+      )}
     </div>
   );
 }
