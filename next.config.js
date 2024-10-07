148b1b36f3129e61e0c69aa0e6c841e74431a86a
guddnboy <hung3018@naver.com>
2024-10-07 20:00:36 +0900

    chore: export 수정

--- next.config.js
+++ next.config.js
@@ -4,11 +4,12 @@ const nextConfig = {
   images: {
     domains: ["api.dicebear.com"],
   },
-  output: "export",
+
   assetPrefix:
     process.env.NODE_ENV === "production"
       ? "https://guddnboy.github.io/chat-image-generator/"
       : "",
-};
 
+  output: "export",
+};
 module.exports = nextConfig;
